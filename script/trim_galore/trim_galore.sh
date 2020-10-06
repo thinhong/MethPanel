d73e8bb944f016e7b35c975f4f613982206e1fd8
thinhong <34956551+thinhong@users.noreply.github.com>
2020-10-06 18:25:38 +0700

    Update trim_galore.sh

--- script/trim_galore/trim_galore.sh
+++ script/trim_galore/trim_galore.sh
@@ -9,12 +9,12 @@ trim_galore="/path/to/trim_galore/0.6.5/trim_galore"
 # trimming
 if [[ "$isSingleEnd" -eq "1" ]]; then
 	# trim single end
-	"$trim_galore" -q 30 --illumina --length 20 -o "$O" --clip_R1 1 --three_prime_clip_R1 1 --gzip --fastqc --fastqc_args "--outdir $O" --trim-n "$R1"
+	"$trim_galore" -q $BASE_QUALITY --illumina --length $READ_LENGTH -o "$O" --clip_R1 1 --three_prime_clip_R1 1 --gzip --fastqc --fastqc_args "--outdir $O" --trim-n "$R1"
 	# rename after timmed
 	for i in `find "$O" -name "*.fq.gz"`; do mv "$i" "${i/_R1_trimmed.fq.gz/_trimmed_R1.fastq.gz}"; done
 else
 	# trim paired end
-	$trim_galore -q 30 --illumina --length 20 -o "$O" --clip_R1 1 --clip_R2 1 --three_prime_clip_R1 1 --three_prime_clip_R2 1 --gzip --fastqc --fastqc_args "--outdir $O" --trim-n --paired "$R1" "$R2"
+	$trim_galore -q $BASE_QUALITY --illumina --length $READ_LENGTH -o "$O" --clip_R1 1 --clip_R2 1 --three_prime_clip_R1 1 --three_prime_clip_R2 1 --gzip --fastqc --fastqc_args "--outdir $O" --trim-n --paired "$R1" "$R2"
 	# rename after timmed
 	for i in `find "$O" -name "*_R1_trimmed.fq.gz"`; do mv "$i" "${i/_R1_trimmed.fq.gz/_trimmed_R1.fastq.gz}"; done
 	for i in `find "$O" -name "*_R2_trimmed.fq.gz"`; do mv "$i" "${i/_R2_trimmed.fq.gz/_trimmed_R2.fastq.gz}"; done
